0bc1d762c24f43194735de36d8acc94e5183f601
Pamela Castillo Vega <pamela.castillo.vega@gmail.com>
2022-08-24 13:38:02 +0800

    seeded users and listsing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,34 @@
 require "faker"
 
+LISTING_CAT = ["Cafe", "Coworking Space", "Restaurant", "Residential"]
+BOOKING_CAT = ["Table", "Room"]
 
-# LISTING_CAT = ["Cafe", "Coworking Space", "Restaurant", "Residential"]
-# BOOKING_CAT = ["Table", "Room"]
+# Seeding users and listings
+puts "Cleaning up database..."
+User.destroy_all
+Listing.destroy_all
 
-
-# 10.times do
-#   name: Faker::Company.name
-#   Address: Faker::Address.street_address
-#   Listing Type:
-#   Booking Type:
-#   Description: Faker::Company.catch_phrase
-#   Price: sample
-# end
+puts "Seeding database..."
+10.times do
+  user = User.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: "password"
+  )
+  puts "User created"
+  # Create a random number (between 0-5) of seeds per user
+  rand(5).times do
+    Listing.create(
+      name: Faker::Company.name,
+      address: Faker::Address.street_address,
+      listing_type: LISTING_CAT.sample,
+      booking_type: BOOKING_CAT.sample,
+      description: Faker::Company.catch_phrase,
+      price: rand(2..20),
+      user:
+    )
+    puts "Successfully created"
+  end
+end
+puts "Finished!"
