6e1b8ce32755eb4ac7c3a4a85d39f7d886009281
Abdul Rafi <arafi15@gmail.com>
2022-08-25 12:15:31 +0800

    cloudinary upload completed

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -40,6 +40,6 @@ class ListingsController < ApplicationController
   private
 
   def listing_params
-    params.require(:listing).permit(%i[name address listing_type booking_type description price zone])
+    params.require(:listing).permit(:name, :address, :listing_type, :booking_type, :description, :price, :zone, photos: [])
   end
 end
