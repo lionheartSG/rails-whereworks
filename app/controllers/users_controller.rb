28fa93ae89448354a7f5838887b3b908c2248af0
Fairul Izwan <izwanrul11@gmail.com>
2022-08-25 14:53:36 +0800

    done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,16 @@
 class UsersController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[show]
+
   def show
-    @user = current_user
-    authorize @user
+    if user_signed_in?
+      @user = current_user
+      authorize @user
+      @active = @user.bookings.where(status: "Active")
+      @pending = @user.bookings.where(status: "Pending")
+      @completed = @user.bookings.where(status: "Completed")
+      raise
+    else
+      redirect_to root_path
+    end
   end
 end
