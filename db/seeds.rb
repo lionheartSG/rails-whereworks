10ec0a28ffe727c383fafd8aa51c48c3b739403a
Pamela Castillo Vega <pamela.castillo.vega@gmail.com>
2022-08-29 15:30:49 +0800

    updated user review seeds, added styling to booking show

--- db/seeds.rb
+++ db/seeds.rb
@@ -64,6 +64,17 @@ listing_workbuddy = Listing.create(
 )
 puts 'Created listing'
 
+book = Booking.create(
+  date: '10/09/22',
+  start_time: '12:00 PM',
+  end_time: '1:00 PM',
+  quantity: rand(1..5),
+  status: 'Pending',
+  listing: listing_workbuddy,
+  user: user_bob
+)
+puts 'Booking created'
+
 book1 = Booking.create(
   date: '29/08/22',
   start_time: '12:00 PM',
@@ -74,6 +85,13 @@ book1 = Booking.create(
   user: user_yc
 )
 puts 'Booking created'
+
+UserReview.create(
+  user_rating: 1,
+  user_comment: "What's up with this guy... he brought his own lunch!",
+  booking: book
+)
+puts 'User Review created'
 
 ListingReview.create(
   listing_rating: 1,
@@ -151,7 +169,7 @@ Booking.create(
   quantity: rand(1..5),
   status: 'Active',
   listing: listing_sb_fu,
-  user: user_anna,
+  user: user_anna
 )
 puts 'Booking created'
 
